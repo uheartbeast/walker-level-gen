e65d29086b1461fe1124cc6f838113f87d682ba3
Benjamin Anderson <heartbeast.studios@gmail.com>
2020-07-17 11:50:41 -0600

    Added rooms.

--- Walker.gd
+++ Walker.gd
@@ -16,8 +16,9 @@ func _init(starting_position, new_borders):
 	borders = new_borders
 
 func walk(steps):
+	create_room(position)
 	for step in steps:
-		if randf() <= 0.25 or steps_since_turn >= 4:
+		if steps_since_turn >= 6:
 			change_direction()
 		
 		if step():
@@ -36,6 +37,7 @@ func step():
 		return false
 
 func change_direction():
+	create_room(position)
 	steps_since_turn = 0
 	var directions = DIRECTIONS.duplicate()
 	directions.erase(direction)
@@ -44,6 +46,14 @@ func change_direction():
 	while not borders.has_point(position + direction):
 		direction = directions.pop_front()
 
+func create_room(position):
+	var size = Vector2(randi() % 4 + 2, randi() % 4 + 2)
+	var top_left_corner = (position - size/2).ceil()
+	for y in size.y:
+		for x in size.x:
+			var new_step = top_left_corner + Vector2(x, y)
+			if borders.has_point(new_step):
+				step_history.append(new_step)
 
 
 
--- World.gd
+++ World.gd
@@ -10,7 +10,7 @@ func _ready():
 
 func generate_level():
 	var walker = Walker.new(Vector2(19, 11), borders)
-	var map = walker.walk(500)
+	var map = walker.walk(200)
 	walker.queue_free()
 	for location in map:
 		tileMap.set_cellv(location, -1)
