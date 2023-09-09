016a1d414372100e7af3bca0ff6c8790c8930474
ZachAR3 <zacharyschwentor@gmail.com>
2023-09-09 13:03:23 +0530

    Updated to work with Godot 4

--- Player.gd
+++ Player.gd
@@ -1,6 +1,7 @@
-extends KinematicBody2D
+extends CharacterBody2D
 
 func _physics_process(_delta):
 	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	var y_input = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	move_and_slide(Vector2(x_input, y_input)*100)
+	set_velocity(Vector2(x_input, y_input)*100)
+	move_and_slide()
